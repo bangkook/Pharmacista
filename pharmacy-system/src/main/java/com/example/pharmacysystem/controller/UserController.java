b8419e992c6a7a3695b307ee5cd40ff3fa4cf7e8
Mariam Hossam <m.hossam2551@gmail.com>
2023-11-15 00:43:28 +0200

    PHR-27 Create user class and dependants

---
+++ pharmacy-system/src/main/java/com/example/pharmacysystem/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.example.pharmacysystem.controller;
+
+import com.example.pharmacysystem.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@CrossOrigin()
+public class UserController {
+    @Autowired
+    private UserService userService;
+}
---
+++ pharmacy-system/src/main/java/com/example/pharmacysystem/service/UserService.java
@@ -0,0 +1,5 @@
+package com.example.pharmacysystem.service;
+
+
+public interface UserService {
+}
