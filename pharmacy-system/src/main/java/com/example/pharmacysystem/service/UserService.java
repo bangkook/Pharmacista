3b9442ad00ad7dfa3d1b800eb0a9394ca43f7b25
Mariam Hossam <m.hossam2551@gmail.com>
2023-11-15 23:21:50 +0200

    Add methods for handling user profile

--- pharmacy-system/src/main/java/com/example/pharmacysystem/service/UserService.java
+++ pharmacy-system/src/main/java/com/example/pharmacysystem/service/UserService.java
@@ -1,5 +1,14 @@
 package com.example.pharmacysystem.service;
 
 
+import com.example.pharmacysystem.model.User;
+
 public interface UserService {
+    User getUserById(int id);
+
+    boolean updateUserData(int id, String address, String city, String country, String zipCode, String phone);
+
+    boolean uploadProfilePicture(int id, String profilePicture);
+
+    boolean changePassword(int id, String currentPassword, String newPassword);
 }
