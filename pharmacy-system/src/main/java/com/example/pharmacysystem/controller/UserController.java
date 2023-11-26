29f3ec5f9134737c3303b99710f89bbc24015179
Mariam Hossam <m.hossam2551@gmail.com>
2023-11-26 16:00:05 +0200

    Write user profile controller api methods

--- pharmacy-system/src/main/java/com/example/pharmacysystem/controller/UserController.java
+++ pharmacy-system/src/main/java/com/example/pharmacysystem/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.example.pharmacysystem.controller;
 
+import com.example.pharmacysystem.model.User;
 import com.example.pharmacysystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/user")
@@ -12,4 +15,54 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @GetMapping("get-user/{id}")
+    @ResponseBody
+    public ResponseEntity<User> getUser(@PathVariable("id") int userId) {
+        try {
+            User user = userService.getUserById(userId);
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PatchMapping("/update-data/{id}")
+    public ResponseEntity<String> updateUserData(@PathVariable("id") int userId,
+                                                 @RequestBody Map<String, String> data) {
+        boolean updated = userService.updateUserData(userId,
+                data.get("streetAddress"),
+                data.get("city"),
+                data.get("country"),
+                data.get("zipCode"),
+                data.get("phoneNumber"));
+        if (updated) {
+            return new ResponseEntity<>("User data updated successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Failed to update user data", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PatchMapping("/upload-profile-picture/{id}")
+    public ResponseEntity<String> uploadProfilePicture(@PathVariable("id") int userId,
+                                                       @RequestParam String profilePicture) {
+        boolean uploaded = userService.uploadProfilePicture(userId, profilePicture);
+        if (uploaded) {
+            return new ResponseEntity<>("Profile picture uploaded successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Failed to upload. User not found", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PatchMapping("/change-password/{id}")
+    public ResponseEntity<String> changePassword(@PathVariable("id") int userId,
+                                                 @RequestParam String currentPassword,
+                                                 @RequestParam String newPassword) {
+        boolean changedPassword = userService.changePassword(userId, currentPassword, newPassword);
+        if (changedPassword) {
+            return new ResponseEntity<>("Password changed successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Current password is wrong", HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+    }
 }
