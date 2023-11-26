9807a0d741f8080ed51ff17a692787d959d2156d
Mariam Hossam <m.hossam2551@gmail.com>
2023-11-26 15:58:48 +0200

    Add PasswordEncoder bean

--- pharmacy-system/src/main/java/com/example/pharmacysystem/PharmacySystemApplication.java
+++ pharmacy-system/src/main/java/com/example/pharmacysystem/PharmacySystemApplication.java
@@ -2,12 +2,20 @@ package com.example.pharmacysystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class PharmacySystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PharmacySystemApplication.class, args);
-	}
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    public static void main(String[] args) {
+        SpringApplication.run(PharmacySystemApplication.class, args);
+    }
 
 }
