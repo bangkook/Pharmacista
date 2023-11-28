acdefb1d119915cc27f11435438d58512adecc7e
emansamir2 <51717395+emansamir2@users.noreply.github.com>
2023-11-29 01:29:39 +0200

    PHR-4 service test cases

--- pharmacy-system/src/main/java/com/example/pharmacysystem/service/UserService.java
+++ pharmacy-system/src/main/java/com/example/pharmacysystem/service/UserService.java
@@ -1,5 +1,12 @@
 package com.example.pharmacysystem.service;
 
 
+import com.example.pharmacysystem.model.User;
+
+import java.util.List;
+
 public interface UserService {
+    public User getUser(String userName, String password);
+    public int checkUser(String userName, String password);
+
 }
---
+++ pharmacy-system/src/test/java/com/example/pharmacysystem/service/logInServiceTest.java
@@ -0,0 +1,182 @@
+package com.example.pharmacysystem.service;
+
+import com.example.pharmacysystem.model.User;
+import com.example.pharmacysystem.repository.UserRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class logInServiceTest {
+    @Autowired
+    private UserService userService;
+    @MockBean
+    private UserRepository userRepository;
+
+    @Test
+    public void getUser_ReturnWantedUser(){
+        User firstUser= new User(1, "eman", "123",
+                false, "16 avennue", "newYork",
+                "usa", "1245","01123186969",
+                "profile_picture_url");
+        User secondUser=new User(2, "Neso", "3211",
+                false, "15 avennue", "newYork",
+                "usa", "1245","01123186969",
+                "profile_picture_url");
+        List<User> Users=List.of( firstUser , secondUser);
+        Mockito.when(userRepository.findAll()).thenReturn(Users);
+
+        User getUser=userService.getUser("eman","123");
+        assertEquals(getUser,firstUser);
+    }
+
+    @Test
+    public void getUser_ReturnNullAsUserNotFound(){
+        User firstUser= new User(1, "eman", "123",
+                false, "16 avennue", "newYork",
+                "usa", "1245","01123186969",
+                "profile_picture_url");
+        User secondUser=new User(2, "Neso", "3211",
+                false, "15 avennue", "newYork",
+                "usa", "1245","01123186969",
+                "profile_picture_url");
+
+        List<User> Users=List.of( firstUser , secondUser);
+        Mockito.when(userRepository.findAll()).thenReturn(Users);
+
+        User getUser=userService.getUser("Maria","1423");
+        assertEquals(getUser,null);
+    }
+    @Test
+    public void getUser_ReturnNullAsUserNameNull(){
+        User firstUser= new User(1, "eman", "123",
+                false, "16 avennue", "newYork",
+                "usa", "1245","01123186969",
+                "profile_picture_url");
+        User secondUser=new User(2, "Neso", "3211",
+                false, "15 avennue", "newYork",
+                "usa", "1245","01123186969",
+                "profile_picture_url");
+
+        List<User> Users=List.of( firstUser , secondUser);
+        Mockito.when(userRepository.findAll()).thenReturn(Users);
+
+        User getUser=userService.getUser(null,"1423");
+        assertEquals(getUser,null);
+    }
+    @Test
+    public void getUser_ReturnNullAsPasswordNull(){
+        User firstUser= new User(1, "eman", "123",
+                false, "16 avennue", "newYork",
+                "usa", "1245","01123186969",
+                "profile_picture_url");
+        User secondUser=new User(2, "Neso", "3211",
+                false, "15 avennue", "newYork",
+                "usa", "1245","01123186969",
+                "profile_picture_url");
+
+        List<User> Users=List.of( firstUser , secondUser);
+        Mockito.when(userRepository.findAll()).thenReturn(Users);
+
+        User getUser=userService.getUser("eman",null);
+        assertEquals(getUser,null);
+    }
+    @Test
+    public void getUser_ReturnNullAsUserNamePasswordNull(){
+        User firstUser= new User(1, "eman", "123",
+                false, "16 avennue", "newYork",
+                "usa", "1245","01123186969",
+                "profile_picture_url");
+        User secondUser=new User(2, "Neso", "3211",
+                false, "15 avennue", "newYork",
+                "usa", "1245","01123186969",
+                "profile_picture_url");
+
+        List<User> Users=List.of( firstUser , secondUser);
+        Mockito.when(userRepository.findAll()).thenReturn(Users);
+
+        User getUser=userService.getUser(null,null);
+        assertEquals(getUser,null);
+    }
+
+    @Test
+    public void checkUser_Return1_UserNameAndPasswordCorrect(){
+        User firstUser= new User(1, "eman", "123",
+                false, "16 avennue", "newYork",
+                "usa", "1245","01123186969",
+                "profile_picture_url");
+        User secondUser=new User(2, "Neso", "3211",
+                false, "15 avennue", "newYork",
+                "usa", "1245","01123186969",
+                "profile_picture_url");
+        List<User> Users=List.of( firstUser , secondUser);
+        Mockito.when(userRepository.findAll()).thenReturn(Users);
+
+        int check=userService.checkUser("eman","123");
+        assertEquals(check,1);
+    }
+
+    @Test
+    public void checkUser_ReturnNegative1_UserNameNotfound(){
+        User firstUser= new User(1, "eman", "123",
+                false, "16 avennue", "newYork",
+                "usa", "1245","01123186969",
+                "profile_picture_url");
+        User secondUser=new User(2, "Neso", "3211",
+                false, "15 avennue", "newYork",
+                "usa", "1245","01123186969",
+                "profile_picture_url");
+
+        List<User> Users=List.of( firstUser , secondUser);
+        Mockito.when(userRepository.findAll()).thenReturn(Users);
+
+        int check=userService.checkUser("Maria","1423");
+        assertEquals(check,-1);
+    }
+    @Test
+    public void checkUser_Return0_wrongPassword(){
+        User firstUser= new User(1, "eman", "123",
+                false, "16 avennue", "newYork",
+                "usa", "1245","01123186969",
+                "profile_picture_url");
+        User secondUser=new User(2, "Neso", "3211",
+                false, "15 avennue", "newYork",
+                "usa", "1245","01123186969",
+                "profile_picture_url");
+
+        List<User> Users=List.of( firstUser , secondUser);
+        Mockito.when(userRepository.findAll()).thenReturn(Users);
+
+        int check=userService.checkUser("eman","666");
+        assertEquals(check,0);
+    }
+    @Test
+    public void checkUser_ReturnNegative1_NullPasswordOrNameOrBoth(){
+        User firstUser= new User(1, "eman", "123",
+                false, "16 avennue", "newYork",
+                "usa", "1245","01123186969",
+                "profile_picture_url");
+        User secondUser=new User(2, "Neso", "3211",
+                false, "15 avennue", "newYork",
+                "usa", "1245","01123186969",
+                "profile_picture_url");
+
+        List<User> Users=List.of( firstUser , secondUser);
+        Mockito.when(userRepository.findAll()).thenReturn(Users);
+
+        int check1=userService.checkUser(null,"666");
+        assertEquals(check1,-1);
+        int check2=userService.checkUser("eman",null);
+        assertEquals(check2,-1);
+        int check3=userService.checkUser(null,null);
+        assertEquals(check3,-1);
+    }
+}
