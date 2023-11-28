972c559c6fdfa9fd1e262dbf5c53040d8e5e8047
Maria Bassem <maria.bassem2002@gmail.com>
2023-11-29 01:36:04 +0200

    PHR-2 Test cases for the registration service

--- pharmacy-system/src/main/java/com/example/pharmacysystem/service/UserService.java
+++ pharmacy-system/src/main/java/com/example/pharmacysystem/service/UserService.java
@@ -1,5 +1,9 @@
 package com.example.pharmacysystem.service;
-
-
+import com.example.pharmacysystem.model.User;
+import org.springframework.stereotype.Service;
+@Service
 public interface UserService {
+    public User saveUser(User user);
+    public User findByUsername(String username);
+    public boolean isUsernameFound(String username);
 }
