56677061b3a303c58dff89c89c93e2f71db1e0bd
emansamir2 <51717395+emansamir2@users.noreply.github.com>
2023-11-29 20:54:51 +0200

    routing

--- pharmacy-frontend/src/App.js
+++ pharmacy-frontend/src/App.js
@@ -1,19 +1,25 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import GoogleSignIn from './components/login';
 import GoogleSignUp from './components/googleSignUp';
 import LoginBasic from './components/LoginBasic';
-import React from 'react';
 import Signup from './components/signup';
-//<LoginBasic/>
-//<GoogleSignIn />
+import AccountSettings from './components/user_profile/UserProfile'
+
 function App() {
   return (
-    <div className='App'>
-    <Signup/>
-      
-    </div>
+    <Router>
+      <div className='App'>
+        <Routes>
+          <Route path='/googleLogin' element={<GoogleSignIn/>} />
+          <Route path='/googleSignUp' element={<GoogleSignUp/>} />
+          <Route path='/LoginBasic' element={<LoginBasic/>} />
+          <Route path='/signup' element={<Signup/>} />
+          <Route path='/Account' element={<AccountSettings/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
 export default App;
-
-
