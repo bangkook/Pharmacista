5c7572e5413e9baa0f2026f0c184776e5f77b8c4
emansamir2 <51717395+emansamir2@users.noreply.github.com>
2023-11-29 04:51:22 +0200

    Revert "PHR-8 BL"
    This reverts commit 5c8c22b9aef56bb7e7fc8863767af0564028e821.

--- pharmacy-system/src/main/java/com/example/pharmacysystem/service/UserServiceImpl.java
+++ pharmacy-system/src/main/java/com/example/pharmacysystem/service/UserServiceImpl.java
@@ -1,44 +1,12 @@
 package com.example.pharmacysystem.service;
 
-import com.example.pharmacysystem.model.User;
 import com.example.pharmacysystem.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
-
-    @Override
-    public User getUser(String userName, String password) {
-        List<User> Users= userRepository.findAll();
-        if(userName==null || password==null) return null;
-        User user=null;
-        for(User u:Users){
-            if(u.getUsername().equals(userName) && u.getPassword().equals(password)) {
-                user = u;
-                break;
-            }
-        }
-        return user;
-    }
-
-    public int checkUser(String userName, String password){
-        List<User> Users= userRepository.findAll();
-        if(userName==null || password==null) return -1;
-        for(User u:Users){
-            if(u.getUsername().equals(userName) ) {
-                if (u.getPassword().equals(password)) {
-                    return 1;//user found and correct password
-                }
-                return 0; //user found but incorrect password
-            }
-        }
-        return -1;//user not in database
-    }
-
 
 }
