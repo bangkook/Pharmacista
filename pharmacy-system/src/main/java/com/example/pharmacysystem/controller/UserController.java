0125526d26c31fe066c07a47d46dd792717d0e6b
emansamir2 <51717395+emansamir2@users.noreply.github.com>
2023-11-29 02:51:41 +0200

    PHR-4 log-in controller test

--- pharmacy-system/src/main/java/com/example/pharmacysystem/controller/UserController.java
+++ pharmacy-system/src/main/java/com/example/pharmacysystem/controller/UserController.java
@@ -1,15 +1,25 @@
 package com.example.pharmacysystem.controller;
 
+import com.example.pharmacysystem.model.User;
 import com.example.pharmacysystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/user")
 @CrossOrigin()
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @GetMapping("/getUserByName")
+    public User getUsersByName(@RequestParam String userName, @RequestParam String password){
+        return null;
+    }
+    @GetMapping("/checkUser")
+    public int checkUser(@RequestParam String userName, @RequestParam String password){
+        return 0;
+    }
 }
