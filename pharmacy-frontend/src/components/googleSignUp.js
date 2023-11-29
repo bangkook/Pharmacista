78878e076cc2659c38f2cbd70b8a46332e7ed2dd
sara-gaballa <saragaballa2002@gmail.com>
2023-11-29 02:54:17 +0200

    PHR-18 Google registration FE

---
+++ pharmacy-frontend/src/components/googleSignUp.js
@@ -0,0 +1,13 @@
+// Front-end component (e.g., GoogleSignUp.js)
+import React from 'react';
+
+export default function GoogleSignUp() {
+
+  return (
+    <div className="center-container">
+      <button type="button" className="google-sign-in-button">
+        Sign up with Google
+      </button>
+    </div>
+  );
+}
