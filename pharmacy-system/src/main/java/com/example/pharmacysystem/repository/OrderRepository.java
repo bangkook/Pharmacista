ab2c26100a2c3d25399031670265dbf34588b450
bangkook <m.hossam2551@gmail.com>
2023-12-08 02:14:06 +0200

    Add queries

--- pharmacy-system/src/main/java/com/example/pharmacysystem/repository/OrderRepository.java
+++ pharmacy-system/src/main/java/com/example/pharmacysystem/repository/OrderRepository.java
@@ -2,8 +2,16 @@ package com.example.pharmacysystem.repository;
 
 import com.example.pharmacysystem.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+
+    @Query(
+            value = "Select * From orders where user_id = ?1",
+            nativeQuery = true)
+    List<Order> getOrdersForUser(int userId);
 }
