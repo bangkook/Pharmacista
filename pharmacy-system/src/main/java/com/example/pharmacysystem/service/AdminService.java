3457fef742974b91bec9be319289b57f9705389e
Maria Bassem <maria.bassem2002@gmail.com>
2023-12-08 09:41:27 +0200

    Admin Service and its class

---
+++ pharmacy-system/src/main/java/com/example/pharmacysystem/service/AdminService.java
@@ -0,0 +1,20 @@
+package com.example.pharmacysystem.service;
+
+import com.example.pharmacysystem.model.User;
+
+import java.util.List;
+
+public interface AdminService {
+    List<User> viewUsers();
+
+    List<User> viewAdmins();
+
+    boolean promoteUser(int userId);
+
+    boolean addProduct(String SerialNumber);
+
+    boolean updateProduct(String SerialNumber);
+
+    boolean deleteProduct(String SerialNumber);
+
+}
---
+++ pharmacy-system/src/main/java/com/example/pharmacysystem/service/AdminServiceImpl.java
@@ -0,0 +1,37 @@
+package com.example.pharmacysystem.service;
+
+import com.example.pharmacysystem.model.User;
+
+import java.util.List;
+
+public class AdminServiceImpl extends UserServiceImpl implements AdminService {
+    @Override
+    public List<User> viewUsers() {
+        return null;
+    }
+
+    @Override
+    public List<User> viewAdmins() {
+        return null;
+    }
+
+    @Override
+    public boolean promoteUser(int userId) {
+        return false;
+    }
+
+    @Override
+    public boolean addProduct(String SerialNumber) {
+        return false;
+    }
+
+    @Override
+    public boolean updateProduct(String SerialNumber) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteProduct(String SerialNumber) {
+        return false;
+    }
+}
