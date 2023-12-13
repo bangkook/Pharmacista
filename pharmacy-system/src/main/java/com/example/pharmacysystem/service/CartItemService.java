472376486af739d6da2eb49c3dc1f166e33b7875
NancyHisham <96303049+NancyHisham@users.noreply.github.com>
2023-12-14 01:58:28 +0200

    PHR-40 CartIem BL

--- pharmacy-system/src/main/java/com/example/pharmacysystem/service/CartItemService.java
+++ pharmacy-system/src/main/java/com/example/pharmacysystem/service/CartItemService.java
@@ -4,8 +4,16 @@ import com.example.pharmacysystem.model.CartItem;
 import com.example.pharmacysystem.utils.CartItemId;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface CartItemService {
 
-    boolean saveCartItem(CartItem item);
+    void updateQuantityByUserNameAndSerialNumber(int userId, String productSN, int quantity);
+
+    List<CartItem> getCartItemsByUserId(int userId);
+
+    boolean deleteCartItem(CartItemId cartItemId);
+
+    boolean deleteCartItemByproductSN(String SerialNumber);
 }
--- pharmacy-system/src/main/java/com/example/pharmacysystem/service/CartItemServiceImpl.java
+++ pharmacy-system/src/main/java/com/example/pharmacysystem/service/CartItemServiceImpl.java
@@ -1,24 +1,61 @@
 package com.example.pharmacysystem.service;
 
 import com.example.pharmacysystem.model.CartItem;
+import com.example.pharmacysystem.model.Product;
 import com.example.pharmacysystem.repository.CartItemRepository;
+import com.example.pharmacysystem.repository.ProductRepository;
 import com.example.pharmacysystem.utils.CartItemId;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CartItemServiceImpl implements CartItemService{
     @Autowired
     private CartItemRepository cartItemRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
+
 
     @Override
-    public boolean saveCartItem(CartItem item) {
+    public void updateQuantityByUserNameAndSerialNumber(int userId, String productSN, int quantity){
+        CartItem cartItem = cartItemRepository.findByUserNameAndSerialNumber(userId, productSN);
+
+        if (cartItem != null) {
+            cartItem.setQuantity(quantity);
+            cartItemRepository.save(cartItem);
+        }
+
+    }
+
+    @Override
+    public List<CartItem> getCartItemsByUserId(int userId){
+        return cartItemRepository.getCartItemsByUserId(userId);
+    }
+
+    @Override
+    public boolean deleteCartItem(CartItemId cartItemId) {
         try {
-            cartItemRepository.save(item);
+
+            cartItemRepository.deleteById(cartItemId);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    @Transactional
+    public boolean deleteCartItemByproductSN(String SerialNumber){
+        try {
+            cartItemRepository.deleteCartItemByproductSN(SerialNumber);
             return true;
         } catch (Exception e) {
             e.printStackTrace();
