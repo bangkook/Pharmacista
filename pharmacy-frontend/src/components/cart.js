586c73ac9ea7971022faec5381c8e95f1592bfa9
NancyHisham <96303049+NancyHisham@users.noreply.github.com>
2023-12-14 02:45:37 +0200

    PHR-42 Rest API

--- pharmacy-frontend/src/components/cart.js
+++ pharmacy-frontend/src/components/cart.js
@@ -2,7 +2,235 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function ShoppingCart() {
+  const userId = 1
+  //const serialNumbers = [1, 2, 3];
+  const [medicines, setMedicines] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [isCartEmpty, setIsCartEmpty] = useState(true); 
+  const [totalPrice, setTotalPrice] = useState(0);
 
+  const fetchMedicines = async () => {
+    
+    try {
+      const response = await axios.get(`http://localhost:8088/Product-From-Cart?userId=${userId}`);
+      setMedicines(response.data);
+      console.log(response.data);
+  
+      // Use map to update each product in the response
+      const updatedCart = response.data.map((product) => {
+        let quantity;
+        if(product.amount == 0){
+          handleDeleteItem(product.productSN);
+        }
+        else if (product.quantity > product.amount) {
+          quantity = product.amount;
+        }
+        else {
+          quantity = product.quantity;
+        }
+  
+        return {
+          ...product,
+          productName: product.productName, // Corrected from product.Name
+          quantity: quantity,
+        };
+      });
+  
+      // Initialize the cart with default quantities and max amounts
+      setCart(updatedCart);
+    } catch (error) {
+      console.error('Error fetching medicines:', error);
+    }
+  };
+  
+  
+  useEffect(() => {
+    fetchMedicines();
+  }, []);
+  
+
+  const handleQuantityChange = async (serialNumber, newQuantity) => {
+    try {
+      // Make an API call to fetch the max amount from the back-end
+      const maxAmountResponse = await axios.get(`http://localhost:8088/get-max-amount?serialNumber=${serialNumber}`);
+      const maxAmount = maxAmountResponse.data;
+      console.log(maxAmount)
+      
+      if(maxAmount == 0){
+        handleDeleteItem(serialNumber);
+        alert(`This product is out-of-stock`);
+      }
+      else{
+
+        if(newQuantity > maxAmount){
+          newQuantity = maxAmount
+          alert(`Cannot exceed the maximum amount: ${maxAmount}`);
+        }
+      
+        // Check if the new quantity exceeds the max amount
+        if (newQuantity <= maxAmount) {
+          setCart((prevCart) =>
+            prevCart.map((item) =>
+              item.serialNumber === serialNumber ? { ...item, quantity: newQuantity } : item
+            )
+          );
+
+          try {
+            await axios.put(`http://localhost:8088/Update-Quantity`, {
+              userId: userId, // Replace with the actual user ID
+              productSN: serialNumber,
+              quantity: newQuantity,
+            });
+            fetchMedicines();
+            setCart((prevCart) =>
+              prevCart.map((item) =>
+                item.serialNumber === serialNumber ? { ...item, quantity: newQuantity } : item
+              )
+            );
+
+
+          } catch (error) {
+            console.error('Error updating quantity:', error);
+          }
+
+        } 
+      
+        else {
+          console.log(`Cannot exceed the maximum amount: ${maxAmount}`);
+          alert(`Cannot exceed the maximum amount: ${maxAmount}`);
+        }
+      }
+      } catch (error) {
+        console.error('Error fetching max amount:', error);
+      }
+  };
+
+  const deleteCartItem = async (serialNumber) => {
+    try {
+      const response = await fetch(`http://localhost:8088/removeCartItem?userId=${userId}&productSN=${serialNumber}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        const data = await response.text();
+        console.log(data);
+      } else {
+        console.error('CartItem not found or deletion failed:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error deleting:', error.message);
+    }
+  };
+  
+
+  const handleDeleteItem = (serialNumber) => {
+    deleteCartItem(serialNumber)
+      .then(() => fetchMedicines()) // Fetch medicines only after successful deletion
+      .catch((error) => console.error('Error deleting item:', error));
+  };
+
+  const calculateTotalPrice = () => {
+    return cart.reduce((total, item) => {
+      return total + item.quantity * item.price;
+    }, 0);
+  };
+  
+
+  const handleConfirmPurchase = async () => {
+    try {
+      // Fetch the updated cart data from the server
+      const updatedCartResponse = await axios.get(`http://localhost:8088/Product-From-Cart?userId=${userId}`);
+      const updatedCart = updatedCartResponse.data;
+  
+      // Compare the current cart with the updated cart to check for changes
+      const cartChanges = cart.some((item) => {
+        const updatedItem = updatedCart.find((updatedItem) => updatedItem.productSN === item.productSN);
+        return !updatedItem || updatedItem.quantity < item.quantity;
+      });
+  
+      if (cartChanges) {
+        alert('Cart has been updated. Please review your items before confirming the purchase.');
+        fetchMedicines(); // Refresh the cart to reflect the changes
+        return;
+      }
+    
+        
+        const orderInfo = {
+            userId: userId,
+            dateCreated: new Date(),
+            totalPrice: parseFloat(calculateTotalPrice()).toFixed(2)
+        };
+        console.log("orderInfo: "+orderInfo);
+
+        const response = await axios.post('http://localhost:8088/createOrder', orderInfo);
+        console.log(response.status);
+
+        if (response.status === 200) {
+            const orderId = response.data;
+            console.log('Order ID:', orderId);
+
+            for (const item of cart) {
+                const orderDetail = {
+                    orderId: orderId,
+                    productSN: item.productSN,
+                    quantity: item.quantity
+                };
+
+                try {
+                    const orderDetailResponse = await axios.post('http://localhost:8088/createOrderDetails', orderDetail);
+                    console.log(orderDetailResponse.status);
+                    console.log(orderDetailResponse.data);
+
+                    const requestData = {
+                        serialNumber: item.productSN, // Fix the key to match the server-side
+                        quantity: item.quantity
+                    };
+
+                    try {
+                        const updateResponse = await axios.put('http://localhost:8088/UpdateOrderdProducts', requestData);
+                        console.log(updateResponse.status);
+                        console.log(updateResponse.data);
+                        if(updateResponse.data == "empty"){
+                          alert(`${item.productName} only has amount = ${item.quantity}`)
+                          fetchMedicines();
+
+                        }
+                        else if(updateResponse.data == "outOfStock"){
+                          alert(`${item.productName} is outOfStock`)
+                          handleDeleteItem(item.productSN);
+                        }
+                        else{
+                          alert(`successfully Purchase!`)
+                          handleDeleteItem(item.productSN);
+                        }
+
+                    } catch (error) {
+                        console.error('Error updating quantity:', error.message);
+                    }
+
+                } catch (orderDetailError) {
+                    console.error('Error creating order detail:', orderDetailError.message);
+                }
+            }
+
+        } else {
+            console.error('Done:', response.status);
+        }
+
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+  
+};
+
+
+  useEffect(() => {
+    // Update isCartEmpty whenever cart changes
+    setIsCartEmpty(cart.length === 0);
+  }, [cart]);
   
 
   return (
---
+++ pharmacy-system/src/main/java/com/example/pharmacysystem/controller/CartController.java
@@ -0,0 +1,132 @@
+package com.example.pharmacysystem.controller;
+import com.example.pharmacysystem.model.CartItem;
+import com.example.pharmacysystem.model.Order;
+import com.example.pharmacysystem.model.OrderDetail;
+import com.example.pharmacysystem.model.Product;
+import com.example.pharmacysystem.service.CartItemService;
+import com.example.pharmacysystem.service.OrderDetailService;
+import com.example.pharmacysystem.service.OrderService;
+import com.example.pharmacysystem.service.ProductService;
+import com.example.pharmacysystem.utils.CartItemId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000") // Replace with your frontend URL
+public class CartController {
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private CartItemService cartItemService;
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private OrderDetailService orderDetailService;
+
+    @GetMapping("get-max-amount")
+    @ResponseBody
+    public ResponseEntity<Integer> getProductQuantity(@RequestParam("serialNumber") String serialNumber) {
+        Product product = productService.getProductBySerialNumber(serialNumber);
+        if (product != null && product.getQuantity() > 0) {
+            return new ResponseEntity<>(product.getQuantity(), HttpStatus.OK);
+        } else
+            return new ResponseEntity<>(0, HttpStatus.OK);
+    }
+
+
+    @GetMapping("Product-From-Cart")
+    @ResponseBody
+    public List<Map<String, Object>> getProductsAndQuantities(@RequestParam("userId") int userId) {
+        List<Map<String, Object>> result = new ArrayList<>();
+
+        List<CartItem> cartItems = cartItemService.getCartItemsByUserId(userId);
+
+        for (CartItem cartItem : cartItems) {
+            Map<String, Object> productInfo = new HashMap<>();
+            productInfo.put("productSN", cartItem.getProductSN());
+            productInfo.put("quantity", cartItem.getQuantity());
+            
+            Product product = productService.getProductBySerialNumber(cartItem.getProductSN());
+
+            if (product != null && product.getQuantity() > 0) {
+                productInfo.put("productName", product.getName());
+                productInfo.put("price", product.getPrice());
+                productInfo.put("photo", product.getPhoto());
+                productInfo.put("amount", product.getQuantity());
+                result.add(productInfo);
+                // Add more product information as needed
+            }else if (product == null){ //The product is deleted from the inventory
+                cartItemService.deleteCartItemByproductSN(cartItem.getProductSN());
+            }
+
+        }
+        System.out.println("result: "+ result);
+        return result;
+    }
+    @PutMapping("Update-Quantity")
+    @ResponseBody
+    public void updateQuantity(@RequestBody Map<String, Object> payload) {
+        int userId = (int) payload.get("userId");
+        String productSN = (String) payload.get("productSN");
+        int quantity = (int) payload.get("quantity");
+        cartItemService.updateQuantityByUserNameAndSerialNumber(userId, productSN, quantity);
+        System.out.println("quantity: " + quantity);
+    }
+
+    @DeleteMapping("/removeCartItem")
+    @ResponseBody
+    public ResponseEntity<String> deleteCartItem(@RequestParam int userId, @RequestParam String productSN) {
+        CartItemId cartItemId = new CartItemId(userId,productSN);
+        if (cartItemService.deleteCartItem(cartItemId)) {
+            return new ResponseEntity<>("CartItem deleted successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("CartItem not found or deletion failed", HttpStatus.NOT_FOUND);
+        }
+    }
+    @PostMapping("/createOrder")
+    @ResponseBody
+    public ResponseEntity<Integer> createOrder(@RequestBody Order order) {
+        System.out.println("order: "+order);
+        if (orderService.createOrder(order)) {
+            return new ResponseEntity<>(order.getId(), HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+    @PostMapping("/createOrderDetails")
+    @ResponseBody
+    public ResponseEntity<String> createOrderDetails(@RequestBody OrderDetail Orderdetail) {
+        if (orderDetailService.createOrderDetails(Orderdetail)) {
+            return new ResponseEntity<>("order created successfully", HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>("order failed to create", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("UpdateOrderdProducts")
+    @ResponseBody
+    public ResponseEntity<String> UpdateOrderdProducts(@RequestBody Map<String, Object> request) {
+        String serialNumber = (String) request.get("serialNumber");
+        int quantity = (int) request.get("quantity");
+        String response = productService.updateQuantityBySerialNumber(serialNumber, quantity);
+        if (response == "Success") {
+            return new ResponseEntity<>("Successfully ordered", HttpStatus.OK);
+        } else if(response == "empty"){
+            return new ResponseEntity<>("empty", HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>("outOfStock", HttpStatus.OK);
+        }
+    }
+
+}
