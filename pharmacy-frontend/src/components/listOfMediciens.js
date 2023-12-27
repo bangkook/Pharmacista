a519e6a150aefdef829e3653f392d1c49b42cd25
emansamir2 <51717395+emansamir2@users.noreply.github.com>
2023-12-28 00:54:02 +0200

    PHR-70 remove unused imports

--- pharmacy-frontend/src/components/listOfMediciens.js
+++ pharmacy-frontend/src/components/listOfMediciens.js
@@ -3,14 +3,8 @@ import { Button, TextField } from "@mui/material";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import ImageListItemBar from "@mui/material/ImageListItemBar";
-import Box from "@mui/material/Box";
 import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
-import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 
 const Search = styled('div')(({ theme }) => ({
