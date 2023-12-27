1588e6cd16571e788250561f8dcb4ca855696704
Maria Bassem <maria.bassem2002@gmail.com>
2023-12-27 18:08:04 +0200

    Review UI

---
+++ pharmacy-frontend/src/components/Reviews/reviewForm.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import './reviewForm.css'; // Import the CSS file
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import reviewService from '../../services/ReviewService.js';
+ 
+
+const ReviewForm = () => {
+  const [feedback, setFeedback] = useState({
+    rating: '0',
+    comment: '',
+    productSN: '123456789123456858',
+    userId: '1'
+  });
+
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFeedback((prevFeedback) => ({ ...prevFeedback, [name]: value }));
+  };
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  const handleStarClick = (selectedRating) => {
+    setFeedback((prevFeedback) => ({
+        ...prevFeedback,
+        rating: prevFeedback.rating === selectedRating ? selectedRating - 1 : selectedRating,
+      }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log('Submitted feedback:', feedback);
+    try {
+      await reviewService.saveReview(feedback);
+      console.log('Review saved successfully!');
+    } catch (error) {
+      console.error('Error saving review:', error);
+    }
+  };
+
+  return (
+    <div className="container">
+      {!isModalOpen && <button onClick={openModal}>Open Feedback Form</button>}
+      {isModalOpen && (
+        <div className="modal-overlay">
+          <div className="modal-content">
+            <div className="feedback-form">
+              <h2>Provide Your Feedback</h2>
+              <form onSubmit={handleSubmit}>
+                <div className="star-rating">
+                  <label>Rating:</label>
+                  <div>
+                    {[...Array(5)].map((_, index) => (
+                      <span
+                        key={index}
+                        className={`star ${index + 1 <= feedback.rating ? 'active' : ''}`}
+                        onClick={() => handleStarClick(index + 1)}
+                      >
+                        <i className="fas fa-star"></i>
+                      </span>
+                    ))}
+                  </div>
+                </div>
+
+                <div>
+                  <label htmlFor="comment">Comment:</label>
+                  <textarea
+                    id="comment"
+                    name="comment"
+                    value={feedback.comment}
+                    onChange={handleInputChange}
+                  />
+                </div>
+
+                <button type="submit">Submit Feedback</button>
+              </form>
+            </div>
+            <button onClick={closeModal}>Close Modal</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ReviewForm;
---
+++ pharmacy-frontend/src/services/ReviewService.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const REVIEWS_BASE_URL = 'http://localhost:8088/reviews'; // Replace with your backend API base URL
+
+const reviewService = {
+  saveReview: async (review) => {
+    try {
+      const response = await axios.post(`${REVIEWS_BASE_URL}/save`, review);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  getReviewsForProduct: async (productSN) => {
+    try {
+      const response = await axios.get(`${REVIEWS_BASE_URL}/product/${productSN}`);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  },
+};
+
+export default reviewService;
