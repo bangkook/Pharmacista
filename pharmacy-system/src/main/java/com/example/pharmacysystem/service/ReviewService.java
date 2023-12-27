43ae3c4146ff60df943246fc5def594edea7e3f6
Maria Bassem <maria.bassem2002@gmail.com>
2023-12-28 01:12:27 +0200

    Adding the DTO in the service

--- pharmacy-system/src/main/java/com/example/pharmacysystem/service/ReviewService.java
+++ pharmacy-system/src/main/java/com/example/pharmacysystem/service/ReviewService.java
@@ -1,10 +1,13 @@
 package com.example.pharmacysystem.service;
 
+import com.example.pharmacysystem.dto.ReviewDTO;
 import com.example.pharmacysystem.model.Review;
 
 import java.util.List;
 
 public interface ReviewService {
     Review saveReview(Review review);
-    List<Review> getReviewsForProduct(String productSN);
+    List<ReviewDTO> getReviewsForProduct(String productSN);
+
+    ReviewDTO  convertToDTO(Review review);
 }
--- pharmacy-system/src/main/java/com/example/pharmacysystem/service/ReviewServiceImpl.java
+++ pharmacy-system/src/main/java/com/example/pharmacysystem/service/ReviewServiceImpl.java
@@ -1,16 +1,23 @@
 package com.example.pharmacysystem.service;
 
+import com.example.pharmacysystem.dto.ReviewDTO;
 import com.example.pharmacysystem.model.Review;
+import com.example.pharmacysystem.model.User;
 import com.example.pharmacysystem.repository.ReviewRepository;
+import com.example.pharmacysystem.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ReviewServiceImpl implements ReviewService {
     @Autowired
     private ReviewRepository reviewRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     public Review saveReview(Review review) {
@@ -18,7 +25,27 @@ public class ReviewServiceImpl implements ReviewService {
     }
 
     @Override
-    public List<Review> getReviewsForProduct(String productSN) {
-        return reviewRepository.findByProductSN(productSN);
+    public List<ReviewDTO> getReviewsForProduct(String productSN) {
+        List<Review> reviewList = reviewRepository.findByProductSN(productSN);
+        return reviewList.stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public ReviewDTO convertToDTO(Review review) {
+        User user = userRepository.findById(review.getUserId()).orElse(null);
+        if (user != null) {
+            return new ReviewDTO(
+                    review.getReviewId(),
+                    review.getUserId(),
+                    review.getProductSN(),
+                    review.getRating(),
+                    review.getComment(),
+                    review.getReviewDate(),
+                    user.getUsername());
+        } else {
+            return null;
+        }
     }
 }
