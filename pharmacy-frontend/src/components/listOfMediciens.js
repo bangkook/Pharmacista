0279938052a1fe4dc032ca008e7803f0bca31987
emansamir2 <51717395+emansamir2@users.noreply.github.com>
2023-12-27 20:07:23 +0200

    PHR-67 frontend

--- pharmacy-frontend/src/components/listOfMediciens.js
+++ pharmacy-frontend/src/components/listOfMediciens.js
@@ -1,4 +1,4 @@
-import { Grid, Paper,Avatar, TextField, Button, Typography ,Link} from "@mui/material";
+import { Grid, Paper,Avatar, TextField, Button,  Card, CardContent, Modal, Typography ,Link} from "@mui/material";
 import React, { useState }  from "react";
 import { useEffect } from 'react';
 import IconButton from '@mui/material/IconButton';
@@ -12,13 +12,34 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { wait } from "@testing-library/user-event/dist/utils";
 
+import { styled } from '@mui/system';
+const StyledImage = styled('img')({
+  width: '100%',
+  height: '210px',
+  objectFit: 'cover',
+  transition: 'opacity 0.3s ease-in-out, filter 0.3s ease-in-out',
+  '&:hover': {
+    cursor: 'pointer',
+    opacity: 0.85,
+    filter: 'brightness(0.7)',
+  }
+});
+
 const ListOfMediciens=({userId})=>{
   console.log(userId)
   const BaseUri = 'http://localhost:8088'
   const [initialMedicines, setInitialMedicines] = useState([])
   const [cart, setCart] = useState([])
   const [products, setProducts] = useState([])
+  const [selectedMedicine, setSelectedMedicine] = useState(null);
+  
+  const handleMedicineClick = (medicine) => {
+    setSelectedMedicine(medicine);
+  };
 
+  const handleCloseModal = () => {
+    setSelectedMedicine(null);
+  };
 
   const getListOfMediciens = async () => {
     try {
@@ -132,44 +153,87 @@ const ListOfMediciens=({userId})=>{
   }
   return (
     <div>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static" sx={{ backgroundColor: '#2e2d88' }}>
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'center' }}>
-              Medicines List
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </Box>
-      <ImageList sx={{ width: '100%', height: '100%' }} cols={5} gap={8}>
+        <ImageList sx={{ width: '100%', height: '100%'}} cols={5}>
         {initialMedicines.map((item) => (
-          <ImageListItem key={item.serialNumber}>
-            <Paper elevation={3} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-              <img
-                src={item.photo}
-                alt={item.name}
-                className="medicine-image"
-                style={{ maxWidth: '100%', maxHeight: '150px' }}
-              />
-              <ImageListItemBar title={item.name} subtitle={<span>Price: {item.price}</span>} position="below" />
-              <Button
+            <ImageListItem key={item.serialNumber}>
+            <StyledImage
+              src={item.photo}
+              alt={item.name}
+              onClick={() => handleMedicineClick(item)}
+            />
+            <ImageListItemBar title={item.name} subtitle={<span>Price: {item.price}</span>} position="below" />
+            <Button
                 onClick={() => addToCart(item.serialNumber)}
                 variant="contained"
-                size="small"
                 style={{
+                color: 'white',
+                backgroundColor: cart.some((cartItem) => cartItem === item.serialNumber)
+                    ? '#a6192e'
+                    : '#2e2d88',
+                }}
+            >
+                {cart.some((cartItem) => cartItem=== item.serialNumber) ? 'Remove from Cart' : 'Add to Cart'}
+            </Button>
+            </ImageListItem>
+        ))}
+        </ImageList>
+        <Modal
+        open={Boolean(selectedMedicine)}
+        onClose={handleCloseModal}
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+      <Box sx={{ width: '55%', height: '75%', bgcolor: 'background.paper', p: 2 }}>
+      {selectedMedicine && (
+        <div>
+          <Box display="flex">
+            <Box display="flex" flexDirection="column" alignItems="center">
+              <img
+                alt={selectedMedicine.name}
+                src={selectedMedicine.photo}
+                style={{ objectFit: 'cover', height: '250px', width: '250px', marginBottom: '12px' }}
+              />
+              <Button
+                onClick={() => addToCart(selectedMedicine.serialNumber)}
+                variant="contained"
+                style={{
+                  width: '100%',
                   color: 'white',
-                  backgroundColor: cart.some((cartItem) => cartItem === item.serialNumber) ? '#a6192e' : '#2e2d88',
+                  backgroundColor: cart.some((cartItem) => cartItem === selectedMedicine.serialNumber) ? '#a6192e' : '#2e2d88',
                 }}
               >
-                {cart.some((cartItem) => cartItem === item.serialNumber) ? 'Remove' : 'Add'}
+                {cart.some((cartItem) => cartItem === selectedMedicine.serialNumber) ? 'Remove from Cart' : 'Add to Cart'}
               </Button>
-            </Paper>
-          </ImageListItem>
-        ))}
-      </ImageList>
+            </Box>
+            <CardContent>
+              <Typography variant="h5" component="div" style={{ marginBottom: '12px' }}>
+                {selectedMedicine.name}
+              </Typography>
+              <Typography variant="body2" color="text.secondary" style={{ marginBottom: '12px' }}>
+                Price: {selectedMedicine.price}
+              </Typography>
+              <Typography variant="body1" component="div" style={{ marginBottom: '12px' }}>
+                {selectedMedicine.description}
+              </Typography>
+              <Typography variant="body2" color="text.secondary" style={{ marginBottom: '12px' }}>
+                Production date: {selectedMedicine.productionDate}
+              </Typography>
+              <Typography variant="body2" color="text.secondary" style={{ marginBottom: '12px' }}>
+                Expiry date: {selectedMedicine.expiryDate}
+              </Typography>
+            </CardContent>
+          </Box>
+        </div>
+      )}
+    </Box>
+      </Modal>
     </div>
-  );
-};
+    );
+  }
+  
 
 
 
